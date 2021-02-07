2a0e41a8cf86d1aab1778de555a7235b12e0c805
davidracovan <74030080+davidracovan@users.noreply.github.com>
2021-02-07 09:41:27 -0500

    help test

--- bot/main.py
+++ bot/main.py
@@ -12,7 +12,7 @@ from bot.cogs.info import Info
 # https://discord.com/api/oauth2/authorize?client_id=796805491186597968&permissions=2147483639&scope=bot
 
 def start():
-    bot = commands.Bot(command_prefix='c?', help_command=None)
+    bot = commands.Bot(command_prefix='c?')
     bot.add_cog(Events(bot))
     bot.add_cog(Suggestions(bot))
     bot.add_cog(School(bot))
