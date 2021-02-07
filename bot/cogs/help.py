965d8c44a4cb0c791e28bd2eded7df64b860f456
davidracovan <74030080+davidracovan@users.noreply.github.com>
2021-02-07 10:36:12 -0500

    help test

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -3,10 +3,8 @@ from discord.ext import commands
 from bot import tools
 
 from bot.cogs.suggestions import Suggestions
-from bot.cogs.events import Events
 from bot.cogs.school import School
 from bot.cogs.fun import Fun
-from bot.cogs.help import Help
 from bot.cogs.info import Info
 
 class Help(commands.Cog):
@@ -22,15 +20,16 @@ class Help(commands.Cog):
             embed.add_field(name='Fun Commands', value=f'`{ctx.prefix}help fun`', inline=False)
             embed.add_field(name='Informational Commands', value=f'`{ctx.prefix}help info`', inline=False)
             embed.add_field(name='School Commands', value=f'`{ctx.prefix}help school`', inline=False)
+            embed.add_field(name='Suggestions Commands', value=f'`{ctx.prefix}help suggestions`', inline=False)
             await ctx.send(embed=embed)
 
     @help.command(name='fun')
     async def _fun(self, ctx):
         embed = tools.create_embed(ctx, 'Fun Commands')
         for command in Help.get_commands():
-            embed.add_field(name=f'{ctx.prefix}{command.name}', value='Allows you to greet the bot.', inline=False)
-            embed.add_field(name=f'{ctx.prefix}8ball <question>', value='Ask a question, get an answer.', inline=False)
-            embed.add_field(name=f'{ctx.prefix}rng <minimum> <maximum>', value='Generate a random number between two numbers.', inline=False)
+            embed.add_field(name=f'{ctx.prefix}{command.name}', value=command.description, inline=False)
+            print(command.signature)
+            print(command.usage)
         await ctx.send(embed=embed)
 
     @help.command(name='info')
@@ -40,6 +39,15 @@ class Help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(name='school')
+    async def _school(self, ctx):
+        embed = tools.create_embed(ctx, 'School Commands')
+        embed.add_field(name=f'{ctx.prefix}register <blue day lunch> <gold day lunch> <cohort>', value="Example: `chs_register B D greyhound`\nAllows you to register details with the bot to get personalized responses.\nAll three values are required.\nOther commands will currently not work without registration.", inline=False)
+        embed.add_field(name=f'{ctx.prefix}schoolday [all]', value="Tells you information about today (Blue/Gold, In Person/Virtual, Late Start, weekends, breaks, etc.).\nThe `all` argument is optional, and it will display information for both cohorts.", inline=False)
+        embed.add_field(name=f'{ctx.prefix}schoolweek [all]', value="Tells you information about the next seven days.\nThe `all` argument is optional, and it will display information for both cohorts.", inline=False)
+        embed.add_field(name=f'{ctx.prefix}schooldate <date> [all]', value="Tells you information about a specified date.\nThe `date` argument is required, and must be in the form `mm/dd/yyyy`.\nThe `all` argument is optional, and it will display information for both cohorts.", inline=False)
+        await ctx.send(embed=embed)
+
+    @help.command(name='suggestions')
     async def _school(self, ctx):
         embed = tools.create_embed(ctx, 'School Commands')
         embed.add_field(name=f'{ctx.prefix}register <blue day lunch> <gold day lunch> <cohort>', value="Example: `chs_register B D greyhound`\nAllows you to register details with the bot to get personalized responses.\nAll three values are required.\nOther commands will currently not work without registration.", inline=False)
