b3c825e97d6d9eb2ec061d39eafc5da9b3d9d3c2
davidracovan <74030080+davidracovan@users.noreply.github.com>
2021-02-07 10:37:40 -0500

    fix

--- bot/main.py
+++ bot/main.py
@@ -6,18 +6,18 @@ from bot.cogs.suggestions import Suggestions
 from bot.cogs.events import Events
 from bot.cogs.school import School
 from bot.cogs.fun import Fun
-# from bot.cogs.help import Help
+from bot.cogs.help import Help
 from bot.cogs.info import Info
 
 # https://discord.com/api/oauth2/authorize?client_id=796805491186597968&permissions=2147483639&scope=bot
 
 def start():
-    bot = commands.Bot(command_prefix='c?')
+    bot = commands.Bot(command_prefix='c?', help_command=None)
     bot.add_cog(Events(bot))
     bot.add_cog(Suggestions(bot))
     bot.add_cog(School(bot))
     bot.add_cog(Fun(bot))
-    # bot.add_cog(Help(bot))
+    bot.add_cog(Help(bot))
     bot.add_cog(Info(bot))
     bot.run(os.environ['TOKEN']) # bot token
 
