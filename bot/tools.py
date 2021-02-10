cf4a9f9df95cc71ff732db413ad9ddbc39e0c126
davidracovan <74030080+davidracovan@users.noreply.github.com>
2021-02-10 08:21:30 -0500

    fix ttt

--- bot/tools.py
+++ bot/tools.py
@@ -6,14 +6,14 @@ def create_embed(ctx, title, desc=None, url=None, color=None, footer_enabled=Tru
     embed = discord.Embed(title=title, description=desc, url=url, color=color)
     embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
     if footer_enabled:
-        embed.set_footer(text=f'Server: {ctx.guild} | Command: {ctx.message.content}', icon_url=ctx.guild.icon_url)
+        embed.set_footer(text=f'Server: {ctx.guild} | Command: {ctx.command}', icon_url=ctx.guild.icon_url)
     return embed
 
 def create_error_embed(ctx, desc):
     color = discord.Color.red()
     embed = discord.Embed(title="Error", description=desc, color=color)
     embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-    embed.set_footer(text=f'Server: {ctx.guild} | Command: {ctx.message.content}', icon_url=ctx.guild.icon_url)
+    embed.set_footer(text=f'Server: {ctx.guild} | Command: {ctx.command}', icon_url=ctx.guild.icon_url)
     return embed
 
 def log_command(ctx):
